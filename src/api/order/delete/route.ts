30f61bb86b1ea897c66809f52f21fb563823d548
n-batrakov <n-batrakov@users.noreply.github.com>
2020-06-21 20:55:21 +0300

    Add delete api

---
+++ src/api/order/delete/route.ts
@@ -0,0 +1,19 @@
+import { AppConfig } from "../../server/config";
+import { r, HttpRequest } from "@marblejs/core";
+import { map, mapTo } from "rxjs/operators";
+import { deleteOrderHandler } from "./db";
+import { DeleteOrderRequest } from "./types";
+
+type RouteParams = {
+  id: string,
+}
+
+export const deleteOrderRoute = (config: AppConfig) => r.pipe(
+  r.matchPath('/:id'),
+  r.matchType('DELETE'),
+  r.useEffect($ => $.pipe(
+    map((req: HttpRequest<void, RouteParams, void>) => <DeleteOrderRequest>{ id: req.params.id }),
+    deleteOrderHandler(config),
+    mapTo({ }),
+  ))
+)
--- src/api/server/routes.ts
+++ src/api/server/routes.ts
@@ -4,12 +4,14 @@ import { AppConfig } from './config'
 import { autocompleteRoute } from '../autocomplete/route'
 import { getOrdersRoute } from '../order/get/route'
 import { getOrderByIdRoute } from '../order/getById/route'
+import { deleteOrderRoute } from '../order/delete/route'
 
 export const appRoutes = (config: AppConfig) => [
   combineRoutes('/api', [
     combineRoutes('/order', [
       getOrdersRoute(config),
       getOrderByIdRoute(config),
+      deleteOrderRoute(config),
     ]),
     autocompleteRoute(config),
   ]),
