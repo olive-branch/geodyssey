1b7d90b756008c00cec9b006f563bd601bc1a941
n-batrakov <n-batrakov@users.noreply.github.com>
2020-06-24 17:00:52 +0300

    Refactor commands

--- src/api/order/update/db.ts
+++ src/api/order/update/db.ts
@@ -2,46 +2,8 @@ import { SqlOptions, SqlCommand, fromSqlTransaction } from '../../server/db/opea
 import { pipe, OperatorFunction } from 'rxjs'
 import { UpdateOrderRequest, UpdateOrderResponse } from './types'
 import { map, mergeMap, mapTo } from 'rxjs/operators'
-import { Obj } from '../../../utils'
+import { toUpdateStatement } from '../../server/db/sql'
 
-
-type State = { fields: string[], values: any[] }
-const fieldsReducer = (acc: State, [k, v]: [string, any], i: number): State => {
-  let key = k.toLowerCase()
-  let variable = `$${i + 2}`
-
-  return {
-    fields: [...acc.fields, `"${key}" = ${variable}`],
-    values: [...acc.values, v],
-  }
-}
-
-const isScalar = (x: any) =>
-  x instanceof Date ||
-  typeof x === 'boolean' ||
-  typeof x === 'number' ||
-  typeof x === 'string' ||
-  x === null
-
-const toUpdateStatement = (entity: string, x: Obj | undefined): SqlCommand | null => {
-  if (x === undefined) {
-    return null
-  }
-
-  let { fields, values } = Object
-    .entries(x)
-    .filter(([_, v]) => isScalar(v))
-    .reduce(fieldsReducer, { fields: [] as string[], values: [] as any[] })
-
-  if (fields.length === 0) {
-    return null
-  }
-
-  return {
-    values: [x.id, ...values],
-    text: `UPDATE "${entity}" SET ${fields.join(', ')} WHERE id = $1`,
-  }
-}
 
 const toCommands = (req: UpdateOrderRequest): SqlCommand[] => {
   let commands = [
