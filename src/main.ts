76ae395cdd4c1880401e6678999bbe409b09f774
Xeni Kopa <xeni.kopa@icloud.com>
2020-07-07 14:55:24 +0300

    update datalist, fix data get

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueToast from 'vue-toast-notification';
-import Multiselect from 'vue-multiselect'
 
 import 'vue-toast-notification/dist/theme-default.css';
 import 'vue2-datepicker/index.css';
-import "vue-multiselect/dist/vue-multiselect.min.css";
 
 Vue.use(VueToast, { position: "top-right"});
 Vue.config.productionTip = false;
-Vue.component('multiselect', Multiselect)
 
 new Vue({
   render: h => h(App),
