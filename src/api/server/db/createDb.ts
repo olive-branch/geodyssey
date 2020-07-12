4d0c1219baf1d8875180802d16b9d10b08fa091a
n-batrakov <n-batrakov@users.noreply.github.com>
2020-07-12 20:48:04 +0300

    Add retries to db check

--- src/api/server/db/createDb.ts
+++ src/api/server/db/createDb.ts
@@ -2,8 +2,26 @@ import { AppConfig } from '../config'
 import { Pool } from 'pg'
 import { fromSqlCommand, SqlOptions } from './opearators'
 import { mergeMap, mergeMapTo, tap } from 'rxjs/operators'
-import { empty } from 'rxjs'
+import { empty, from } from 'rxjs'
 import { readFile } from '@etlx/operators/fs'
+
+const sleep = (ms: number) => new Promise(res => setTimeout(res, ms))
+
+const tryConnect = async (config: SqlOptions, attempts: number, delay: number) => {
+  let pool = config.pool()
+  try {
+    await pool.connect()
+  } catch (e) {
+    if (attempts > 1) {
+      await sleep(delay)
+      await tryConnect(config, attempts - 1, delay)
+    } else {
+      throw e
+    }
+  }
+}
+
+const canConnect = (confg: SqlOptions) => from(tryConnect(confg, 50, 1e3))
 
 
 const createDatabase = (config: SqlOptions, name: string) => fromSqlCommand(config, {
@@ -34,7 +52,8 @@ export const initDb = (sqlScriptPath: string, config: AppConfig) => {
 
   console.log('Checking if database exists...')
 
-  return dbExists(opts, name).pipe(
+  return canConnect(opts).pipe(
+    mergeMapTo(dbExists(opts, name)),
     tap(x => console.log(x ? 'Database Exists' : 'Creating new database...')),
     mergeMap(exists => exists
       ? empty()
